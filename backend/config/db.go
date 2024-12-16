34d9441c57f2dd0f5e5c66164e04e62d91ed5b22
Moch. Avin <5025221061@mhs.its.ac.id>
2024-12-16 11:04:15 +0700

    feat: add Dockerfiles for backend and frontend, and update .dockerignore

--- backend/config/db.go
+++ backend/config/db.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,10 +11,10 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	host := os.Getenv("POSTGRES_HOST")
 	user := os.Getenv("POSTGRES_USER")
@@ -31,6 +30,8 @@ func ConnectDB() {
 		DSN:                  dsn,
 		PreferSimpleProtocol: true,
 	}
+
+	var err error
 
 	DB, err = gorm.Open(postgres.New(pgConfig), &gorm.Config{
 		PrepareStmt: false,
